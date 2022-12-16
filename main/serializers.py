7064edf605097562ad3a6a696ec4f55a87d96aa8
Ayup923 <aupmaan@gmail.com>
2022-12-16 12:27:04 +0600

    serializers

---
+++ main/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Post
+from review.serializers import CommentSerializer
+
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def to_representation(self, instance:Post):
+        rep =  super().to_representation(instance)
+        rep["author"] = instance.author.username
+        comments = instance.comments.all()
+        rep['comments'] = CommentSerializer(comments, many=True).data
+        rep['likes'] = instance.likes.count()
+        return rep
